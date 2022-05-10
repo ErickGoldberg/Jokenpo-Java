82c932940a1861b62fa34f7b50fb2359238c5190
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2022-05-10 17:51:08 -0300

    Add files via upload

---
+++ Jokenpo.java
@@ -0,0 +1,13 @@
+package jokenpo;
+import java.util.Scanner;
+public class Jokenpo {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        
+      
+    }
+    
+}
